4074afb97834cdefb9616d4dccedc1cf3aedbd24
= <liamsutton1997@outlook.com>
2017-09-26 14:22:41 +0100

    Bot now responds to simple commands

--- bot.js
+++ bot.js
@@ -1,18 +1,43 @@
 const discord = require('discord.js')
 const token = require('./secret.js')
 
+const commandPrefix = '!'
+
 const client = new discord.Client()
 
 client.on('ready', () => {
     console.log("OU-HELPER-BOT LOADED")
 })
 
+
 client.on('message', message => {
     let sender = message.author
+    let isCommand = message.content.startsWith(commandPrefix)
 
     if (sender.bot) return
 
-    console.log("[INFO]: Message recieved from: " + sender.username + " at: " + message.createdAt.getHours() + ":" + message.createdAt.getMinutes())
+    console.log(`[INFO] Message recieved from ${sender.username}`)
+
+    if (isCommand) {
+        let command = message.content.toUpperCase()
+
+        switch (command) {
+            case '!HI':
+                sender.send(`Hi there ${sender.username}, how are you!?`)
+                break
+            
+            case '!INFO':
+                sender.send(`I am a discord bot built by Sugoi Memes using the Discord.js module! :smiley:`)
+                break
+            
+            case '!SOURCE':
+                sender.send(`I am open source! Github: https://github.com/LiamSutton/discord-bot`)
+                break
+
+            default:
+            sender.send(`Hmm.. I didnt quite get that, to show all my commands type '!help'`)
+        }
+    }
 })
 
 client.login(token)
