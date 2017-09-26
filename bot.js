5e8a048a14a3a1e869189603b2c66be4b4144484
= <liamsutton1997@outlook.com>
2017-09-26 13:49:23 +0100

    Bot now logs when it recieves a message from user

--- bot.js
+++ bot.js
@@ -7,5 +7,12 @@ client.on('ready', () => {
     console.log("OU-HELPER-BOT LOADED")
 })
 
+client.on('message', message => {
+    let sender = message.author
+
+    if (sender.bot) return
+
+    console.log("[INFO]: Message recieved from: " + sender.username + " at: " + message.createdAt.getHours() + ":" + message.createdAt.getMinutes())
+})
 
 client.login(token)
